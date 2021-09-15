e459dba2e09396bc188596204510d139445bbda8
Bharath Kumar <bharathkumar.gajula22@gmail.com>
2021-09-15 12:53:02 +0000

    react-context-part-3

---
+++ src/components/CartItem/index.js
@@ -0,0 +1,62 @@
+import {BsPlusSquare, BsDashSquare} from 'react-icons/bs'
+import {AiFillCloseCircle} from 'react-icons/ai'
+import CartContext from '../../context/CartContext'
+
+import './index.css'
+
+const CartItem = props => {
+  const {cartItemDetails} = props
+  const {id, title, brand, quantity, price, imageUrl} = cartItemDetails
+
+  return (
+    <CartContext.Consumer>
+      {value => {
+        const {deleteCartItem} = value
+
+        const onClickRemoveItem = () => {
+          deleteCartItem(id)
+        }
+
+        return (
+          <li className="cart-item">
+            <img className="cart-product-image" src={imageUrl} alt={title} />
+            <div className="cart-item-details-container">
+              <div className="cart-product-title-brand-container">
+                <p className="cart-product-title">{title}</p>
+                <p className="cart-product-brand">by {brand}</p>
+              </div>
+              <div className="cart-quantity-container">
+                <button type="button" className="quantity-controller-button">
+                  <BsDashSquare color="#52606D" size={12} />
+                </button>
+                <p className="cart-quantity">{quantity}</p>
+                <button type="button" className="quantity-controller-button">
+                  <BsPlusSquare color="#52606D" size={12} />
+                </button>
+              </div>
+              <div className="total-price-delete-container">
+                <p className="cart-total-price">Rs {price * quantity}/-</p>
+                <button
+                  className="remove-button"
+                  type="button"
+                  onClick={onClickRemoveItem}
+                >
+                  Remove
+                </button>
+              </div>
+            </div>
+            <button
+              className="delete-button"
+              type="button"
+              onClick={onClickRemoveItem}
+            >
+              <AiFillCloseCircle color="#616E7C" size={20} />
+            </button>
+          </li>
+        )
+      }}
+    </CartContext.Consumer>
+  )
+}
+
+export default CartItem
